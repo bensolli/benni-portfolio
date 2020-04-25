f6a0ef585a8f52c3e971ccd58e7149194498d940
bensolli <44747432+bensolli@users.noreply.github.com>
2020-04-25 11:12:57 +0200

    adding a homepage/landingpage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import Cards from '../components/cards'
-
+import React from 'react';
+import Popup from '../components/popup'
+import { Link } from 'react-router-dom';
+import Back from "../components/back";
 
 export default () => {
-
     return (
-        <div className="[ Cards ]">
-
+        <div className="col-sm-10">
+            <div className="[ popupwrapper ]">
+                <Popup header='ALERT!' />
+                <div className="[ popupwrapper_content ]">
+                    <h2>Looks like you just entered Benni’s portfolio, would you like to continue?</h2>
+                </div>
+                <div className="[ popupwrapper_buttons ]">
+                    <Back />
+                    <Link to="/Dashboard"><button className="[ popupwrapper_buttons-cta ][ buttons ]">Yes</button></Link>
+                </div>
+            </div>
         </div>
     );
 }
