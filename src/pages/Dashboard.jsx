53023a285ca0af9554ec224a8ea1c90b81c46563
bensolli <44747432+bensolli@users.noreply.github.com>
2020-04-25 11:14:54 +0200

    Making a page for better navigation

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Popup from '../components/popup'
+import { Link } from 'react-router-dom';
+import Back from "../components/back";
+
+export default () => {
+    return (
+        <div className="col-sm-10">
+            <div className="[ popupwrapper ]">
+                <Popup header='What would you like to do?' />
+                <div className="[ popupwrapper_buttons ]">
+                    <Back />
+                    <Link to="/Gallery"><button className="[ popupwrapper_buttons-cta ][ buttons ]">Look at Gallery</button></Link>
+                    <Link to="/Bio"><button className="[ popupwrapper_buttons-cta ][ buttons ]">Look at Bio</button></Link>
+                    <Link to="/Contact"><button className="[ popupwrapper_buttons-cta ][ buttons ]">Contact Benni</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+}
