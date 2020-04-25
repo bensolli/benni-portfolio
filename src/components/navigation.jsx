ee77f2760f9b8ee779e20cafe18ed93c900a77c0
bensolli <44747432+bensolli@users.noreply.github.com>
2020-04-25 11:16:17 +0200

    adding navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default () => {
     return (
+        <div className="col-sm-12">
         <nav>
             <ul className="[ navigation-container ]">
-                <Link to="/"><li className="[ navigation-container__nav-element ]">Homepage</li></Link>
-                <Link to="/About"><li className="[ navigation-container__nav-element ]">About</li></Link>
+                <Link to="Dashboard"><li className="[ navigation-container__nav-element ]">Explore</li></Link>
+                <Link to="Gallery"><li className="[ navigation-container__nav-element ]">Gallery</li></Link>
+                <Link to="/Bio"><li className="[ navigation-container__nav-element ]">BIO</li></Link>
                 <Link to="/Contact"><li className="[ navigation-container__nav-element ]">Contact</li></Link>
+                <Link to="/Backgrounds"><li className="[ navigation-container__nav-element ]">Theme</li></Link>
             </ul>
         </nav>
+        </div>
     )
 }
 
