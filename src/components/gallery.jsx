536ef33d9364b408c85ba2db13905b04248cb85c
bensolli <44747432+bensolli@users.noreply.github.com>
2021-02-17 11:07:17 +0100

    fixing link

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -9,8 +9,8 @@ export default ({ img, desc, links1, links2, linkDescription1, linkDescription2,
                         <p>{desc}</p>
                         <br />
                         <ul>
-                            <li>{linkDescription1} <a href={links1} alt="link">{links1}</a></li>
-                            <li>{linkDescription2} <a href={links2} alt="link">{links2}</a></li>
+                            <li>{linkDescription1} <a href={links1} alt="link" target="_blank" style={{display: "table-cell"}}>{links1}</a></li>
+                            <li>{linkDescription2} <a href={links2} alt="link" target="_blank" style={{display: "table-cell"}}>{links2}</a></li>
                         </ul>
                        
         </>
